b491c5697cb143fe7eb6f47b6b15c6c067193212
wilharty <richwrghtt@gmail.com>
2022-10-21 20:25:02 +0000

    task 0 fixed1

--- _printf.c
+++ _printf.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * check_specifier - check that character is a valid specifier and
- * assigns an appropriate fucntion for its printing.
+ * check_specifier - check that char is
+ * a valid specifier and
+ * assigns an appropriate fucntion for
+ * its printing.
  * @format: the specifier (char*)
  *
- * Return: pointer to function, if successful
+ * Return: pointer to funct if success
  * NULL pointer if not successful
  */
-
 int (*check_specifier(const char *format))(va_list)
 {
 	int i;
@@ -16,16 +17,19 @@ int (*check_specifier(const char *format))(va_list)
 		{"c", print_char},
 		{"s", print_str},
 		{"%", print_cent},
-		//{"i", print_int},
-		//{"d", print_dec},
+		{"i", print_int},
+		{"d", print_dec},
 		{NULL, NULL}
 	};
 
-	for (i = 0; my_array[i].t != NULL; i++)
+	for (i = 0; my_array[i].t !=
+NULL; i++)
 	{
-		if (*(my_array[i].t) == *format)
+		if (*(my_array[i].t) ==
+*format)
 		{
-			return (my_array[i].f);
+			return
+(my_array[i].f);
 		}
 	}
 
@@ -33,52 +37,12 @@ int (*check_specifier(const char *format))(va_list)
 }
 
 /**
- * print_char - prints a character to stdout
+ * print_cent - prints character % to
+ * stdout
  * @args: variadic parameter
  *
  * Return: number of characters printed
  */
-
-int print_char(va_list args)
-{
-	char c = (char)va_arg(args, int);
-	int count = 0;
-
-	if (c)
-	{
-		count = write(1, &c, 1);
-		return (count);
-	}
-
-	return (0);
-}
-
-int print_str(va_list args)
-{
-	int i;
-	int count = 0;
-	char *str;
-
-	str = va_arg(args, char *);
-	if (str == NULL)
-		return (-1);
-
-	while (str[i])
-	{
-		count = write(1, &str[i], 1);
-		i = i + 1;
-	}
-
-	return (count);
-}
-
-/**
- * print_cent - prints character % to stdout
- * @args: variadic parameter
- *
- * Return: number of characters printed
- */
-
 int print_cent(va_list args)
 {
 	(void)args;
@@ -89,13 +53,13 @@ int print_cent(va_list args)
 }
 
 /**
- * print_int - prints a given integer from variadic parameters
+ * print_int - prints a given integer
+ * from variadic parameters
  * @args: variadic parameter
  *
- * Return: number of characters printed on Success
+ * Return: number of characters
+ * printed on Success
  */
-
-/*
 int print_int(va_list args)
 {
 	int i = 0;
@@ -103,39 +67,50 @@ int print_int(va_list args)
 	int numb;
 	int my_arr[10];
 	char x[1];
-*/
-	/*Fetch integer from variadic parameters*/
-//	numb = va_arg(args, int);
 
-	/*Break the numbers apart and assign them to array*/
-/*	while (numb != 0)
+/*
+ * Fetch integer from variadic *
+ * parameters
+ */
+	numb = va_arg(args, int);
+
+/*
+ * Break the numbers apart and
+ * assign them to array
+ */
+	while (numb != 0)
 	{
-		my_arr[i] = (numb % 10);
+		my_arr[i] =
+(numb % 10);
 		numb = numb / 10;
-		if (numb == 0)
-			break;
-		else
-			i++;
+		if (numb != 0)
+			i++
 	}
-*/
-	/*Write the contents of the array to stdout*/
-/*	for (i; i >= 0; i--)
+
+/*
+ * Write the contents of the *
+ * array to stdout
+ */
+	for (i; i >= 0; i--)
 	{
-*/	//	x[0] = ('0' + my_arr[i]); //Convert int to char
-	//	count += write(1, x, 1);
-/*	}
+/*convert int to char*/
+		x[0] =
+('0' + my_arr[i]);
+		count +=
+write(1, x, 1);
+	}
 
 	return (count);
 }
-*/
 
 /**
- * print_dec - prints a given integer from variadic parameters to base 10
+ * print_dec - prints a given integer
+ * from variadic parameters to base 10
  * @args: variadic parameter
  *
- * Return: number of characters printed on Success
+ * Return: number of characters printed
+ * on Success
  */
-/*
 int print_dec(va_list args)
 {
 	int i = 0;
@@ -143,88 +118,89 @@ int print_dec(va_list args)
 	int numb;
 	int my_arr[10];
 	char x[1];
-*/
-	/*Fetch integer from variadic parameters*/
-//	numb = va_arg(args, int);
 
-	/*Break the numbers apart and assign them to array*/
-/*	while (numb != 0)
+/*
+ * Fetch integer from variadic *
+ * parameters
+ */
+	numb = va_arg(args, int);
+
+/*
+ * Break the numbers apart and *
+ * assign them to array
+ */
+	while (numb != 0)
 	{
-		my_arr[i] = (numb % 10);
+		my_arr[i] =
+(numb % 10);
 		numb = numb / 10;
-		if (numb == 0)
-			break;
-		else
+		if (numb != 0)
 			i++;
 	}
-*/
-	/*Write the contents of the array to stdout*/
-/*	for (i; i >= 0; i--)
+
+/*
+ * Write the contents of the *
+ * array to stdout
+ */
+	for (i; i >= 0; i--)
 	{
-		x[0] = ('0' + my_arr[i]); //Convert int to char
-		count += write(1, x, 1);
+/*Convert int to char*/
+		x[0] =
+('0' + my_arr[i]);
+		count +=
+write(1, x, 1);
 	}
 
 	return (count);
 }
-*/
+
 /**
- * _printf - prints a string in a formatted way
+ * _printf - prints a str in a format
  * @format: string to print (char *)
  * @...: variadic parameters (unknown)
  *
  * Return: number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	int count = 0;
-	int value = 0;
-	va_list args;
-	va_start(args, format);
-	int (*f)(va_list);
-	
-	/*Prevent parsing a null pointer*/
-	if (format == NULL)
-		return (-1);
-	
-	/*Print each character of string*/
-	while (format[i])
-	{	
-		if (format[i] != '%')
+int i = 0;
+int count = 0;
+int value = 0;
+va_list args;
+
+va_start(args, format);
+/*Prevent parsing a null pointer*/
+if (format == NULL)
+	return (-1);
+/*Print each character of string*/
+while (format[i])
+{
+	if (format[i] != '%')
+	{
+	value = write(1, &format[i], 1);
+	count = count + value;
+	i++;
+	continue;
+	}
+	if (format[i] == '%')
+	{
+		f = check_specifier(&format[i + 1]);
+
+		if (f != NULL)
 		{
-			value = write(1, &format[i], 1);
-			count = count + value;
-			i++;
-			continue;
+		value = f(args);
+		count = count + value;
+		i = i + 2;
+		continue;
 		}
-
-		if (format[i] == '%')
+		if (format[i + 1] != '\0')
 		{
-			f = check_specifier(&format[i + 1]);
-			if (f != NULL)
-			{
-				value = f(args);
-				count = count + value;
-				i = i + 2;
-				continue;
-			}
-
-			if (format[i + 1] == '\0')
-			{
-				break;
-			}
-
-			if (format[i + 1] != '\0')
-			{
-				value = write(1, &format[i + 1], 1);
-				count = count + value;
-				i = i + 2;
-				continue;
-			}
+		value = write(1, &format[i + 1], 1);
+		count = count + value;
+		i = i + 2;
+		continue;
 		}
 	}
-
+}
 	return (count);
 }
--- main.h
+++ main.h
@@ -6,13 +6,14 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-int (*check_specifier(const char*))(va_list);
+int (*check_specifier(const char *))(va_list);
 
 int print_char(va_list);
 int print_str(va_list);
 int print_cent(va_list);
-//int print_int(va_list);
-//int print_dec(va_list);
+int print_int(va_list);
+int print_dec(va_list);
+int (*f)(va_list);
 
 /**
  * struct func - struct for specifier to printer
@@ -24,5 +25,52 @@ typedef struct func
 	char *t;
 	int (*f)(va_list);
 } func_t;
+
+/**
+ * print_char - prints a character
+ * to stdout
+ * @args: variadic parameter
+ *
+ * Return: number of characters printed
+ */
+int print_char(va_list args)
+{
+	char c = (char)va_arg(args, int);
+	int count = 0;
+
+	if (c)
+	{
+		count = write(1, &c, 1);
+		return (count);
+	}
+
+	return (0);
+}
+
+/**
+ * print_str - prints a string to
+ * stdout
+ * @args: variadic parameter
+ *
+ * Return: number of string printed
+ */
+int print_str(va_list args)
+{
+	int i;
+	int count = 0;
+	char *str;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		return (-1);
+
+	while (str[i])
+	{
+		count = write(1, &str[i], 1);
+		i = i + 1;
+	}
+
+	return (count);
+}
 
 #endif
